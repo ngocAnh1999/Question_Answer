f8d0f9435fdd7341691034e762a269b62770ce63
ngocAnh1999 <anhshmily2512@gmail.com>
2019-11-12 10:14:57 +0700

    add code createSurvey

---
+++ JS/survey/createSurvey.js
@@ -0,0 +1,14 @@
+function createQuestion(question_id) {
+    debugger
+}
+function addMultiAnswer() {
+    debugger
+}
+function addOneAnswer() {
+    debugger
+}
+function onRemove(me) {
+    let nodeRemove = me.parentNode.parentNode;
+    let nodeParent = nodeRemove.parentNode;
+    nodeParent.removeChild(nodeRemove);
+}
